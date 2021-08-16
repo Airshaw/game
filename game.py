e014f0d9ced86ac16f6ff75fda1a43c54a6da115
Airshaw <88904959+Airshaw@users.noreply.github.com>
2021-08-17 01:49:56 +0200

    Update game.py

--- game.py
+++ game.py
@@ -1,10 +1,41 @@
+import game
 from player import Player
-
+import pygame
 
 class Game:
 
     def __init__(self):
         self.player = Player()
-        self.pressed = {
+        self.pressed = { }
+        self.titre=pygame.display.set_caption("comet fall game")
+        self.background = pygame.image.load('assets/bg.jpg')
+        self.screen = pygame.display.set_mode((1080, 720))
 
-        }
+    def run(self):
+        running = True
+        clock = pygame.time.Clock()
+        while running:
+
+            self.screen.blit(self.background, (0, -200))
+            # print(game.pressed)
+            self.screen.blit(self.player.image, self.player.rect)
+            # print(player.rect)
+
+            if self.pressed.get(pygame.K_d) and self.player.rect.x + self.player.rect.width() < self.screen.get_width():
+                self.player.move_right()
+            elif self.pressed.get(pygame.K_q) and self.player.rect.x > 5:
+                self.player.move_left()
+
+            pygame.display.flip()
+
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    running = False
+                    pygame.quit()
+                    print('fermeture')
+
+                elif event.type == pygame.KEYDOWN:
+                    self.pressed[event.key] = True
+                elif event.type == pygame.KEYUP:
+                    self.pressed[event.key] = False
+            clock.tick(60)
