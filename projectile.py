0f34a849fbb069edb90c46bf911666871b2f56a5
Airshaw <88904959+Airshaw@users.noreply.github.com>
2021-08-17 13:09:09 +0200

    projectile

---
+++ projectile.py
@@ -0,0 +1,10 @@
+import pygame
+
+class Projectile(pygame.sprite.Sprite):
+
+    def __init__(self):
+        super(Projectile, self).__init__()
+        self.velocity = 5
+        self.image = pygame.image.load('assets/projectile.png')
+        self.image = pygame.transform.scale(self.image, (50, 50))
+        self.rect = self.image.get_rect()
